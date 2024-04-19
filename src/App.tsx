ab8e59d0cff8a3357c00db634de1bcdf99b8caa7
RileyReidx3 <realkripper@gmail.com>
2024-04-20 00:40:11 +0700

    micellaneous

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import AppRoutes from './routes/AppRoutes';
+import routes from './routes/index';
 
 function App() {
   return (
