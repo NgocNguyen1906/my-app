13a8d7f8f49394635824129b2d6f1246045b8776
RileyReidx3 <realkripper@gmail.com>
2024-04-20 00:56:22 +0700

    update api

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import AppRoutes from './routes/AppRoutes';
-import routes from './routes/index';
 
 function App() {
   return (
